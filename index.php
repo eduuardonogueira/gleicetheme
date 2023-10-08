d341adea944fae4ee6ffcd888b0d2c410b77217c
EduardoNogueiradev <eduardocastroemp@gmail.com>
2023-10-08 20:43:38 -0300

    create page post

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
-    <?php get_header(); ?>
-    <p>Somente um template</p>
-    <?php get_footer(); ?>
+<?php get_header(); ?>
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+        <?php the_title(); ?>
+        <?php the_content(); ?>
+
+    <?php endwhile; else: ?>
+        <p><?php _e('Desculpe, não há posts no momento'); ?></p>
+    <?php endif; ?>
+<?php get_footer(); ?>
 
