65c128b26ad7342a1fd1d2597baa380203ffd9e1
EduardoNogueiradev <eduardocastroemp@gmail.com>
2023-10-08 20:56:12 -0300

    edit blog

--- page-blog.php
+++ page-blog.php
@@ -3,6 +3,13 @@
 ?>
 <?php get_header(); ?>
 
-<p>Página do Blog</p>
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+        <?php the_title(); ?>
+        <?php the_content(); ?>
+
+    <?php endwhile; else: ?>
+        <p><?php _e('Desculpe, não há posts no momento'); ?></p>
+    <?php endif; ?>
 
 <?php get_footer(); ?>
