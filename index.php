b208956093a59c969b701dcb0db45982e401b769
EduardoNogueiradev <eduardocastroemp@gmail.com>
2023-10-08 15:02:20 -0300

    changes

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
-<?php get_header(); ?>
+    <?php get_header(); ?>
+
     <section id="inicio" class="inicio">
         <div class="inicio-imagem"></div>
     
@@ -179,5 +180,6 @@
             <iframe class="contato-mapa" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d788.8814125665016!2d-43.962459304240994!3d-19.857988888638577!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa69067888270c1%3A0xbf6fe233aeb412d!2sAv.%20Cel.%20Jos%C3%A9%20Dias%20Bicalho%2C%20341%20-%2010%20-%20S%C3%A3o%20Luiz%2C%20Belo%20Horizonte%20-%20MG%2C%2031275-050!5e0!3m2!1spt-BR!2sbr!4v1691008097614!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
     </section>
-<?php get_footer(); ?>
+
+    <?php get_footer(); ?>
 
