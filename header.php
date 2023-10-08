ad3c0a7b5b5fad1af37abb66e1a1dc0fd5c6a1dc
EduardoNogueiradev <eduardocastroemp@gmail.com>
2023-10-08 20:22:51 -0300

    finalização do menu

--- header.php
+++ header.php
@@ -27,10 +27,10 @@
                 <a href="/"><img class="menu-logo" src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/logo.webp" alt="Logo dra. Gleice Rodrigues"></a>
 
                 <ul id="menuLista" class="menu-lista up">
-                    <li class="menu-item"><a href="<?php echo (is_front_page()) ? "#inicio" : home_url() ; ?>">Inicio</a></li>
-                    <li class="menu-item"><a href="<?php echo (is_front_page()) ? "#quem somos" : home_url() ; ?>">Quem Somos</a></li>
+                    <li class="menu-item"><a href="<?php echo (is_front_page()) ? "#inicio" : home_url() . "#inicio" ; ?>">Inicio</a></li>
+                    <li class="menu-item"><a href="<?php echo (is_front_page()) ? "#quem somos" : home_url() . "#quem somos" ; ?>">Quem Somos</a></li>
                     <li class="menu-item"><a href="<?php echo (is_front_page()) ? "#areas de atuacao" : home_url() . "#areas de atuacao"; ?>">Áreas de atuação</a></li>
-                    <li class="menu-item"><a href="<?php echo (is_front_page()) ? "#depoimentos" : home_url() ; ?>">Depoimentos</a></li>
+                    <li class="menu-item"><a href="<?php echo (is_front_page()) ? "#depoimentos" : home_url() . "#depoimentos" ; ?>">Depoimentos</a></li>
                     <li class="menu-item"><a href="/contato">Contato</a></li>
                     <li class="menu-item"><a href="/blog">Blog</a></li>
                 </ul>
