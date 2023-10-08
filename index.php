3a277b9a86e2ad8e9b17c402fbae480eeb9c00a9
EduardoNogueiradev <eduardocastroemp@gmail.com>
2023-10-08 13:14:53 -0300

    update script

--- index.php
+++ index.php
@@ -262,6 +262,6 @@
         
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-    <script type="text/javascript" src="./assets/javascript/script.js"></script>
+    <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri() ?>/assets/javascript/script.js"></script>
 </body>
 </html>
