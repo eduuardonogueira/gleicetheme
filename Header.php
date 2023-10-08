c785a0784b8064bfad28eff71584d06e184194eb
EduardoNogueiradev <eduardocastroemp@gmail.com>
2023-10-08 18:56:42 -0300

    att get function to return title name

--- Header.php
+++ Header.php
@@ -14,7 +14,7 @@
         <script src="https://unpkg.com/@phosphor-icons/web"></script>
 
         <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() ?>/style.css">
-        <title> DRA. Gleice Rodrigues | Advocacia & Consultoria </title>
+        <title><?php bloginfo('name'); ?></title>
 
         <!-- Header Wordpress -->
         <?php wp_head(); ?>
