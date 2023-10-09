c0a86a7f8516a9ad08a73cc23886ca7e7c95911f
EduardoNogueiradev <eduardocastroemp@gmail.com>
2023-10-09 13:34:58 -0300

    teste

--- index.php
+++ index.php
@@ -7,18 +7,21 @@
 
             <div class="container-box">
                 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-                <article class="blog-post">
-                    <img src="<?php echo the_post_thumbnail_url()?>" alt="imagem do post">
-
-                    <?php $thumbnail = get_the_post_thumbnail();
-                        echo '<div class="post-thumbnail">' . $thumbnail . '</div>'; ?>
-
-                    <div class="blog-post-content">
-                        <p class="blog-post-date"><?php the_date(); ?></p>
-                        <h1 class="blog-post-title"><?php the_title(); ?></h1>
-                        <p class="blog-post-text"><?php the_excerpt(); ?></p>
-                    </div>
-                </article>
+                    <article class="blog-post">
+                        <img src="<?php echo the_post_thumbnail_url()?>" alt="imagem do post">
+    
+                        <?php $thumbnail = get_the_post_thumbnail();
+                            echo '<div class="post-thumbnail">' . $thumbnail . '</div>'; ?>
+    
+                        <div class="blog-post-content">
+                            <p class="blog-post-date"><?php the_date(); ?></p>
+                            <h1 class="blog-post-title"><?php the_title(); ?></h1>
+                            <?php the_excerpt(); ?>
+                            <a class="blog-post-link" href="<?php the_permalink() ?>">
+                                <button>Leia mais</button>
+                            </a>
+                        </div>
+                    </article>
                 <?php endwhile; else: ?>
                 <p><?php _e('Desculpe, não há posts no momento'); ?></p>
                 <?php endif; ?>
