bd95f6e3bf8498b4da9775e7431e4742dbb07925
EduardoNogueiradev <eduardocastroemp@gmail.com>
2023-10-09 04:21:58 -0300

    tyeste

--- index.php
+++ index.php
@@ -9,10 +9,14 @@
                 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
                 <article class="blog-post">
                     <img src="<?php the_post_thumbnail_url()?>" alt="imagem do post">
+
+                    <?php $thumbnail = get_the_post_thumbnail();
+                        echo '<div class="post-thumbnail">' . $thumbnail . '</div>'; ?>
+
                     <div class="blog-post-content">
                         <p class="blog-post-date"><?php the_date(); ?></p>
                         <h1 class="blog-post-title"><?php the_title(); ?></h1>
-                        <p class="blog-post-text"><?php the_content(); ?></p>
+                        <p class="blog-post-text"><?php the_excerpt(); ?></p>
                     </div>
                 </article>
                 <?php endwhile; else: ?>
