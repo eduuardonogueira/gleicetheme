fb2890b70dbea331c84f908e93cb5c4d1c7c41b1
EduardoNogueiradev <eduardocastroemp@gmail.com>
2023-10-09 18:13:22 -0300

    teste

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
             </div>
 
             <div class="container-box">
-                <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
                     <article class="blog-post">
                         <?php if ( has_post_thumbnail()) {
                              $thumbnail_id = get_post_thumbnail_id();
@@ -24,9 +23,6 @@
                             </a>
                         </div>
                     </article>
-                <?php endwhile; else: ?>
-                <p><?php _e('Desculpe, não há posts no momento'); ?></p>
-                <?php endif; ?>
             </div>
         </section>
 
