114f0d09dd7e52ceba66725bf3d93b17394adae7
EduardoNogueiradev <eduardocastroemp@gmail.com>
2023-10-09 04:05:30 -0300

    att post_date

--- index.php
+++ index.php
@@ -8,9 +8,8 @@
             <div class="container-box">
                 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
                 <article class="blog-post">
-                    <img src="<?php the_post_thumbnail_url() ?>" alt="imagem do post">
+                    <img src="<?php the_post_thumbnail_url()?>" alt="imagem do post">
                     <div class="blog-post-content">
-                        <p class="blog-post-date"><?php get_post_time(); ?></p>
                         <p class="blog-post-date"><?php the_date(); ?></p>
                         <h1 class="blog-post-title"><?php the_title(); ?></h1>
                         <p class="blog-post-text"><?php the_content(); ?></p>
