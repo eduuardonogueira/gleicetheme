1843b80ca9ba8ad48bd433e1c216a5a0f676c3bc
Eduardo Nogueira de Castro <eduardocastroemp@gmail.com>
2023-10-10 18:08:29 -0300

    teste2

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php 
+// Template Name = Blog
+?>
+
 <?php get_header(); ?>
         <section class="blog">
             <div class="blog-background">
@@ -6,16 +10,22 @@
             </div>
 
             <div class="container-box">
+                <?php the_content(); ?> 
+            </div>
+
+            <div class="container-box">
                 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
                     <article class="blog-post">
-                        <?php if ( has_post_thumbnail()) {
-                             $thumbnail_id = get_post_thumbnail_id();
-                             $thumbnail_url = wp_get_attachment_image_src($thumbnail_id, 'full');
+                        <?php 
+                            if ( has_post_thumbnail()) {
+                                $thumbnail_id = get_post_thumbnail_id();
+                                $thumbnail_url = wp_get_attachment_image_src($thumbnail_id, 'full');
                         
-                            echo '<img src="' . $thumbnail_url[0] . '" alt="Imagem de destaque">'; 
-                        } else {
-                            echo '<img src="' . get_stylesheet_directory_uri() .'/assets/img/background-blog.webp" alt="Imagem de destaque">';
-                        }; ?>
+                                echo '<img src="' . $thumbnail_url[0] . '" alt="Imagem de destaque">'; 
+                            } else {
+                                echo '<img src="' . get_stylesheet_directory_uri() .'/assets/img/background-blog.webp" alt="Imagem de destaque">';
+                            }; 
+                        ?>
                         <div class="blog-post-content">
                             <p class="blog-post-date"><?php the_date(); ?></p>
                             <h1 class="blog-post-title"><?php the_title(); ?></h1>
