2a4d80830ced07d852283cb85c562158c35341d7
EduardoNogueiradev <eduardocastroemp@gmail.com>
2023-10-09 22:19:58 -0300

    teste

--- functions.php
+++ functions.php
@@ -20,23 +20,4 @@ if ( function_exists( 'add_image_size' ) ) {
 }
 
 
-/*  Site title
-/* ------------------------------------ */
-function mr_wp_title( $title ) {
-    // Do not filter for RSS feed / if SEO plugin installed
-    if ( is_feed() || class_exists('All_in_One_SEO_Pack') || class_exists('HeadSpace_Plugin') || class_exists('Platinum_SEO_Pack') || class_exists('wpSEO') || defined('WPSEO_VERSION') )
-        return $title;
-    if ( is_front_page() ) { 
-        $title = get_bloginfo('name').' - '.get_bloginfo('description');
-    }
-    if ( is_front_page() && get_bloginfo('description') == '' ) { 
-        $title = get_bloginfo('name');
-    }
-    if ( !is_front_page() ) { 
-        $title .= ' - '.get_bloginfo('name');
-    }
-    return $title;
-}
-add_filter( 'wp_title', 'mr_wp_title' );
-
 ?>
