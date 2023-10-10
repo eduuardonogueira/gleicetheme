a3c0d78612fc210386015ac28844bcc8025098fc
EduardoNogueiradev <eduardocastroemp@gmail.com>
2023-10-10 00:31:29 -0300

    add page-sobre and 404

--- 404.php
+++ 404.php
@@ -1,1 +1,47 @@
-<!-- Criar pagina de erro -->
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="author" content="Gleice Rodrigues">
+        <meta name="description" content="Advocacia e Consultoria">
+        <meta name="keywords" content="advogada, direito, civil, trabalhista, previdenciario, consultoria, juridico">
+        <meta name="copyright" content="© 2023 Gleice Rodrigues">
+        <meta name="robots" content="index,follow">
+        <link rel="shortcut icon" href="assets/img/logo.webp" type="image/x-icon">
+        
+        <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() ?>/assets/css/404.css">
+
+        <title><?php bloginfo('name'); ?></title>
+
+        <!-- Header Wordpress -->
+        <?php wp_head(); ?>
+        <!-- Fecha header Wordpress -->
+    </head>
+
+    <body class="bg-purple">
+        <div class="stars">
+            <div class="central-body">
+                <img class="image-404" src="http://salehriaz.com/404Page/img/404.svg" width="300px">
+                <a href="http://salehriaz.com/404Page/404.html" class="btn-go-home" target="_blank">GO BACK HOME</a>
+            </div>
+            <div class="objects">
+                <img class="object_rocket" src="http://salehriaz.com/404Page/img/rocket.svg" width="40px">
+                <div class="earth-moon">
+                    <img class="object_earth" src="http://salehriaz.com/404Page/img/earth.svg" width="100px">
+                    <img class="object_moon" src="http://salehriaz.com/404Page/img/moon.svg" width="80px">
+                </div>
+                <div class="box_astronaut">
+                    <img class="object_astronaut" src="http://salehriaz.com/404Page/img/astronaut.svg" width="140px">
+                </div>
+            </div>
+            <div class="glowing_stars">
+                <div class="star"></div>
+                <div class="star"></div>
+                <div class="star"></div>
+                <div class="star"></div>
+                <div class="star"></div>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ page-sobre.php
@@ -0,0 +1,8 @@
+<?php 
+// Template Name = Sobre
+?>
+<?php get_header(); ?>
+
+<p> Página em construção </p>
+
+<?php get_footer; ?>
